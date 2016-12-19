66a8bb29b08676614f5eca7ece3458cd15b539c9
chavakishore <chavakishore@gmail.com>
2016-12-18 19:00:12 -0500

    updated jackson mappings and added aws lambda handler

--- src/main/java/com/adiyo/app/movie/config/PersistanceConfig.java
+++ src/main/java/com/adiyo/app/movie/config/PersistanceConfig.java
@@ -52,7 +52,7 @@ public class PersistanceConfig {
     static public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("cineaadio16");
+        config.setPassword("**********");
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
