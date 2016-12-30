9564fc0ae0c69c921ed5d3c4a06d3afe04bc64ab
chavakishore <chavakishore@gmail.com>
2016-12-30 00:23:48 -0500

    changes to build war and deploy to external container

--- src/main/java/com/adiyo/app/movie/LambdaFunctionHandler.java
+++ src/main/java/com/adiyo/app/movie/LambdaFunctionHandler.java
@@ -1,24 +1,11 @@
 package com.adiyo.app.movie;
 
-import com.adiyo.app.movie.model.LambdaRequest;
-import com.adiyo.app.movie.model.Movie;
-import com.adiyo.app.movie.service.MovieService;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.log4j.Logger;
-
-import java.io.IOException;
-import java.util.List;
-
 /**
  * Created by kishorechava on 12/15/16.
  */
-public class LambdaFunctionHandler implements RequestHandler<LambdaRequest, String> {
+public class LambdaFunctionHandler /*implements RequestHandler<LambdaRequest, String>*/ {
 
-    static final Logger log = Logger.getLogger(LambdaFunctionHandler.class);
+    /*static final Logger log = Logger.getLogger(LambdaFunctionHandler.class);
 
     public String handleRequest(LambdaRequest request, Context context)
             throws RuntimeException {
@@ -44,11 +31,11 @@ public class LambdaFunctionHandler implements RequestHandler<LambdaRequest, Stri
             e.printStackTrace();
         }
         return jsonResponse;
-    }
+    }*/
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SpringBootInvocation bootInvocation = new SpringBootInvocation();
         bootInvocation.run();
-    }
+    }*/
 
 }
